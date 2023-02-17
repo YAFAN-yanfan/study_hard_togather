7d5a5d1d689bbcd34b9c2aa82e72892cd0b89382
unknown <3527182911@qq.com>
2023-02-18 00:11:36 +0800

    init repo

---
+++ src/main/java/com/yanfan/study_hard_togather/StudyHardTogatherApplication.java
@@ -0,0 +1,13 @@
+package com.yanfan.study_hard_togather;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyHardTogatherApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudyHardTogatherApplication.class, args);
+    }
+
+}
