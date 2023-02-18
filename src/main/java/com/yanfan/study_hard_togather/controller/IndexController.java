d3ebd220fcc18ebb73d23341be164450d9a8738a
unknown <3527182911@qq.com>
2023-02-18 23:41:39 +0800

    extract github config from code to properties

--- src/main/java/com/yanfan/study_hard_togather/controller/IndexController.java
+++ src/main/java/com/yanfan/study_hard_togather/controller/IndexController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class IndexController {
     @GetMapping("/")
     public String index(){
-        System.out.println("haha");
+
         return  "index";
     }
 }
